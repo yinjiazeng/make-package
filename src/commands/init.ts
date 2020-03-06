764de7eeca110599ea45bf188eafb0cf5860c7b7
Aniu <yinjiazeng@163.com>
2020-03-06 21:48:30 +0800

    init时自动创建目录

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -6,19 +6,28 @@ import { PKG, TEMPLATE_PATH } from '../utils/constant';
 import stringify from '../utils/stringify';
 import print from '../utils/print';
 
-module.exports = (filePath: string, name: string, options: any) => {
-  const pkg = path.join(filePath, PKG);
+module.exports = (filePath: string, name: string = '.', options: any) => {
+  const pkgDir = path.join(filePath, name);
+  const pkg = path.join(pkgDir, PKG);
   const tpkg = path.join(TEMPLATE_PATH, PKG);
+  let dirPath = filePath;
   let tjson = {};
-  let json = { name };
-
-  if (!json.name) {
-    json.name = path.basename(filePath.replace(/\/+$/, ''));
-  }
+  let json = {
+    name: path.basename(pkgDir.replace(/\/+$/, '')),
+  };
 
   try {
     tjson = require(tpkg);
   } catch (e) {}
+
+  name.split('/').forEach((dirName) => {
+    dirPath = path.join(dirPath, dirName)
+    try {
+      fs.accessSync(dirPath);
+    } catch(e) {
+      fs.mkdirSync(dirPath);
+    }
+  });
 
   try {
     fs.accessSync(pkg);
